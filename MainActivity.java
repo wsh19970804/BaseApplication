25570860ebc6a4e5456d1ea1f8bacdfd2c843ffc
wsh19970804 <943935205@qq.com>
2017-05-24 16:00:53 +0800

    非java代码

---
+++ MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.user.baseapplication;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+import com.example.user.baseapplication.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    public void initParms(Bundle parms) {
+
+    }
+
+    @Override
+    public View bindView() {
+        return null;
+    }
+
+    @Override
+    public int bindLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void initView(View view) {
+
+    }
+
+    @Override
+    public void doBusiness(Context mContext) {
+
+    }
+}
